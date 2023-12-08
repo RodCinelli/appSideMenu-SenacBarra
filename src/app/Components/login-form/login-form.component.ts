2f7aabe3092c6e042e46c050233b728c7013f955
RodCinelli <rodcinelli@gmail.com>
2023-12-08 17:00:27 -0300

    Checar o erro do cadastramento

--- src/app/Components/login-form/login-form.component.ts
+++ src/app/Components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth'
 
 @Component({
   selector: 'app-login-form',
@@ -11,19 +12,27 @@ export class LoginFormComponent  implements OnInit {
   mensagem:string=''
   cadUser(email:any, senha:any, rpSenha:any){
     this.mensagem=''
-    if(email!='' || senha!='' || rpSenha!==''){
-      this.mensagem='Preencha todos os campos do formulário!'
-    }else if(senha!=rpSenha){
-      this.mensagem='As senhas precisam ser iguais!'
+    if(email!=='' && senha!=='' && rpSenha!==''){
+      this.mensagem=''
+      
+const error = createUserWithEmailAndPassword(this.auth, email, senha)
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    this.mensagem = errorMessage
+  });
     }else{
-      this.mensagem='Usuário cadastrado com sucesso!'
+      this.mensagem='Preencha todos os campos do formulário!'
     }
   }
 
   Logar(email:any, senha:any){
     console.log(`Usuário: ${email} logado com senha: ${senha} `)
   }
-  constructor() { }
+
+  
+
+  constructor(private auth:Auth) { }
 
   ngOnInit() {}
 
