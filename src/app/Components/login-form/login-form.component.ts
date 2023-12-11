8e9677c8337ac7088dd8f581b43b45561f22a1c8
RodCinelli <rodcinelli@gmail.com>
2023-12-11 16:53:15 -0300

    Subindo o Projeto.

--- src/app/Components/login-form/login-form.component.ts
+++ src/app/Components/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut } from '@angular/fire/auth';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
@@ -8,26 +8,25 @@ import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from
 export class LoginFormComponent implements OnInit {
   cad: boolean = false
   mensagem: string = ''
-  logado: boolean=false
-  isToastOpen = false;
-
+  logado: boolean = false
+  isToastOpen = false
+  user:any={nome:'',foto:''}
   setOpen(isOpen: boolean) {
     this.isToastOpen = isOpen;
   }
-
   cadUser(email: any, senha: any, rpSenha: any) {
     this.mensagem = ''
     if (email == '' || senha == '' || rpSenha == '') {
       this.mensagem = 'Preencha todos os campos do formulário!'
-      this.setOpen(true) 
+      this.setOpen(true)
     } else if (senha != rpSenha) {
       this.mensagem = 'As senhas precisam ser iguas!'
-      this.setOpen(true) 
+      this.setOpen(true)
     } else {
       this.mensagem = 'Usuário cadastrado com sucesso!'
-      this.setOpen(true) 
-      this.cad=!this.cad
-      
+      this.setOpen(true)
+      this.cad = !this.cad
+
       createUserWithEmailAndPassword(this.auth, email, senha)
         .then((userCredential) => {
           // Signed up 
@@ -44,29 +43,47 @@ export class LoginFormComponent implements OnInit {
   }
 
   Logar(email: any, senha: any) {
-  signInWithEmailAndPassword(this.auth, email, senha)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user)
-    this.mensagem=`Usuário: ${user.email} Logado com sucesso!`
-    this.setOpen(true)
-    this.logado=!this.logado
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-}
+    signInWithEmailAndPassword(this.auth, email, senha)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user.email)
+        this.mensagem = `Usuário: ${user.email} logado com sucesso!`
+        this.setOpen(true)
+        this.logado = !this.logado
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
 
-logout() {
-  this.mensagem= 'LogOut efetuado com sucesso!'
+  logout() {
+    this.mensagem = 'LogOut efetuado com sucesso!'
     this.setOpen(true)
-    this.logado=!this.logado
-}
-
-  constructor(private auth:Auth) { }
+    this.logado = !this.logado
+    this.logOutComGoogle()
+  }
+  loginComGoogle() {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(this.auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        this.mensagem = `Usuário: ${result.user.displayName} logado com sucesso!`
+        this.user.nome=result.user.displayName
+        this.user.foto=result.user.photoURL
+        this.setOpen(true)
+        this.logado = !this.logado
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  }
+  logOutComGoogle(){
+    return signOut(this.auth)
+  }
+  constructor(private auth: Auth) { }
 
   ngOnInit() { }
 
